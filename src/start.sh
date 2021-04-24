90a819956c079cf75db48dc8004895a211c7e7f9
hyhgtghyh <81365739+hyhgtghyh@users.noreply.github.com>
2021-04-24 18:13:19 +0100

    sh function finished

--- src/start.sh
+++ src/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 app="docker_mscai"
+cd ..
 docker build -t ${app} .
-nvidia_docker run -it -d -p 5000:5000 \
+nvidia-docker run -it -d -p 5000:5000 \
   --name=${app} \
-  -v $PWD:/home/user/src ${app}
+  -v $PWD/src:/home/user/src ${app}
+docker exec -it ${app} python flasktest.py
